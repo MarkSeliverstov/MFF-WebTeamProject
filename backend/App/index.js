bf27207c324eb241e089a7bce9b097dc6829f563
Mark Seliverstov <96746739+MarkSeliverstov@users.noreply.github.com>
2023-06-05 21:43:07 +0200

    Delete backend directory

--- backend/App/index.js
+++
@@ -1,5 +0,0 @@
-const Crawler = require('./crawler');
-
-let url = "https://webik.ms.mff.cuni.cz/nswi153/";
-let crawler = new Crawler.Crawler();
-crawler.Start(url);
--- backend/Tests/crawler.test.js
+++
@@ -1,9 +0,0 @@
-const crawler = require("../App/crawler");
-
-
-test("simple test", () => {
-    expect(1).toBe(1);
-});
-// test("Crawler", async () => {
-    
-//     });
