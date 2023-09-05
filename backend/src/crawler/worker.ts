9869d4490b25e9a237748f3e43b8ecb19e80804b
Mark Seliverstov <smdmrr00@gmail.com>
2023-09-05 21:02:09 +0200

    add worker

---
+++ backend/src/crawler/worker.ts
@@ -0,0 +1,95 @@
+import { CrowledWebPage } from "../helpers/types";
+import { Crawler } from "./crawler";
+
+export enum Command {
+    RUN,
+    ABORT
+}
+
+export enum Progress {
+    DONE,
+    FAILED,
+    PROGRESS
+}
+
+export type WorkerTask = {
+    url: string,
+    regex: string
+}
+
+export type WorkerCommand = {
+	command: Command.RUN
+	task: WorkerTask;
+} | {
+	command: Command.ABORT
+};
+
+export type WorkerResponse = {
+    status: Progress.DONE
+    result: CrowledWebPage[]
+} | {
+    status: Progress.FAILED,
+    error: string | null
+} | {
+    status: Progress.PROGRESS
+    progress: WorkerProgress
+}
+
+export type WorkerProgress = {
+    crawled: number;
+    total: number,
+    webPages: CrowledWebPage[]
+}
+
+
+/** The main entry point for each crawler worker */
+async function RunTask(
+    task: WorkerTask, 
+    progressCallback: (progress: WorkerProgress) => void
+): Promise<CrowledWebPage[]> {
+	const crawler = new Crawler();
+	currentCrawlers.add(crawler);
+	try {
+		const result = await crawler.StartCrawling(
+            new URL(task.url), new RegExp(task.regex, "ui"), progressCallback
+        );
+        return result;
+	} finally {
+		currentCrawlers.delete(crawler);
+	}
+}
+
+const currentCrawlers = new Set<Crawler>();
+
+// Managment of worker
+console.log(`Crawler ${process.pid} started!`);
+process.on("message", (msg: WorkerCommand) => {
+    if (msg.command === Command.RUN) {
+        console.log(`(crawler ${process.pid}) Started`);
+        RunTask(msg.task, (progress) => {
+            const message: WorkerResponse = {
+                status: Progress.PROGRESS,
+                progress,
+            };
+            process.send?.(message);
+        }).then((result) => {
+            console.log(`[crawler ${process.pid}] Running finished`);
+            const response: WorkerResponse = {
+                status: Progress.DONE,
+                result,
+            };
+            process.send?.(response);
+        }, (error) => {
+            console.log(`[crawler ${process.pid}] Running failed:`, error);
+            const response: WorkerResponse = {
+                status: Progress.FAILED,
+                error: String(error),
+            };
+            process.send?.(response);
+        });
+    }
+    else if (msg.command === Command.ABORT) {
+        console.log(`(crawler ${process.pid}) Aborting...`);
+		currentCrawlers.forEach(c => c.abort());
+    }
+});
