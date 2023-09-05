e8cc94c6c2d0850ff652e8a8967c1551f9ebf45d
Mark Seliverstov <smdmrr00@gmail.com>
2023-09-05 19:40:25 +0200

    remove unused types

--- backend/src/helpers/types.ts
+++ backend/src/helpers/types.ts
@@ -1,51 +1,4 @@
-// Types for the web page data
-export interface crowlingWebPage {
-	url: string
-	title?: string;
-	crawlTimeStart?: number;
-	crawlTimeEnd?: number;
-	links: string[];
-	status: "pending" | "inProgress" | "success"  |  "failed"
-	failedReason?: string
-}
-
-export interface CrawlerProgressCallback{
-	status: "inProgress" | "done";
-	pages: crowlingWebPage[]
-}
-
-export interface webPageExecution {
-	webPage: webPageClientConfig;
-	status: "success" | "queued" | "running" | "failed";
-	message?: string;
-	crawlTime: number;
-	crawledPageCount: number;
-	totalPageCount: number;
-}
-
-export interface webPageClientConfig {
-	identifier: string;
-	/** user given label */
-	label: string;
-	/* where to start crawling */
-	url: string;
-	/* (required)
-  when crawler found a link, it will check if it matches this regexp */
-	regexp: string;
-	/* user given strings */
-	tags: string[];
-	/* if inactive, the site is not crawled based on the Periodicity */
-	active: boolean;
-	/* (minute, hour, day) - how often should the site be crawled */
-	pereodicity: number;
-}
-
-/* How the web pages can be filtered and sorted */
-export type filteredWebPagesViewBy = "url" | "label" | "tags";
-export type sortedWebPagesViewBy = "url" | "crawlTime";
-
 // Types for the exported graphQL data
-
 export type ExportedWebPage = {
 	ididentifier: string;
 	label: string;
@@ -62,3 +15,10 @@ export type ExportedNode = {
 	links: ExportedNode[];
 	owner: ExportedWebPage;
 };
+
+// Other usefull types for crawling
+export type StatusCode = {
+	statusCode: number | "aborted" | "failed" | "skipped"
+}
+
+export type NodeWithStatusCode = ExportedNode & StatusCode
