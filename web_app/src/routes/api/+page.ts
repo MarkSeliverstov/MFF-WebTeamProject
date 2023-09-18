9cd8686511f542254d3937e530da7cc874783736
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2023-09-18 01:56:18 +0000

    Change import to use alias

--- web_app/src/routes/api/+page.ts
+++ web_app/src/routes/api/+page.ts
@@ -1,6 +1,6 @@
 import type { PageLoad } from './$types';
 
-import page from '../../../static/swagger.html?raw';
+import page from '$static/swagger.html?raw';
 
 export const load: PageLoad = async () => {
     return {
