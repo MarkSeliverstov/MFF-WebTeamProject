4a77d7aed55d6c34ad470c07a40656f641800ad6
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2023-09-18 01:50:22 +0000

    Change type interfaces

--- web_app/src/lib/types.ts
+++ web_app/src/lib/types.ts
@@ -11,27 +11,35 @@ export interface CrawledWebPage {
 }
 
 export interface Periodicity {
-	unit: 'minutes' | 'hours' | 'days';
-	interval: number;
+	minutes: number;
+	hours: number;
+	days: number;
 }
 
 export interface WebsiteRecord {
 	id?: ObjectId
 	url: string;
-	periodicity: Periodicity;
+	periodicity?: Periodicity;
 	regexp: string;
 	label: string;
 	active: boolean;
 	tags: string[];
+	latestGroupId: number; // Latest group id that was assigned for execution
 }
 
+// Represents a single web page of a crawling proccess
+// Uniquely identified by the url, ownerId and groupId
 export interface Execution {
-	executionId?: ObjectId;
-	websiteRecordId: ObjectId;
+	id?: ObjectId; // Id for mongo reasons
+	ownerId: ObjectId; // Points to the website record where the crawling started
+	groupId: number; // Belongs to all the pages that were crawled in the same crawling proccess
+	root: boolean; // Start page of crawling
+	url: string; // Url of the page that was crawled
 	crawlTimeStart: Date;
 	crawlTimeEnd: Date;
 	status: 'success' | 'failed' | 'running';
 	sitesCrawled: number;
+	links: string[]; // Links that were found on the page
 }
 
 export abstract class Model<T> {
