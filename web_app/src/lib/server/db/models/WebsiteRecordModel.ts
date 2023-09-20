9ad49fe9030a79e7b0b62db2ea448190ae93f54a
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2023-09-20 21:07:48 +0000

    Implement error handling in the WebsiteRecordModel

--- web_app/src/lib/server/db/models/WebsiteRecordModel.ts
+++ web_app/src/lib/server/db/models/WebsiteRecordModel.ts
@@ -1,20 +1,29 @@
-import { Model, type WebsiteRecord } from '$lib/types';
+import { Model, type WebsiteRecord, type Error } from '$lib/types';
 import { type Collection, ObjectId } from 'mongodb';
 import db from '$db/database';
 
 export default class WebsiteRecordModel extends Model<WebsiteRecord> {
 	collection: Collection = db.collection('webpage_records');
-	async create(websiteRecord: WebsiteRecord): Promise<ObjectId | null> {
+	async create(websiteRecord: WebsiteRecord): Promise<ObjectId | Error> {
 		try {
 			const result = await this.collection.insertOne(websiteRecord);
 			return result.insertedId;
 		} catch (error) {
-			console.log(error);
-			return null;
+			return {
+				code: 500,
+				message: 'Failed to create website record'
+			} as Error;
 		}
 	}
-	async getByID(id: string): Promise<WebsiteRecord | null> {
+	async getByID(id: string): Promise<WebsiteRecord | Error> {
 		try {
+			if (!ObjectId.isValid(id)) {
+				return {
+					code: 400,
+					message: 'Invalid Mongo ID'
+				} as Error;
+			}
+
 			const result = await this.collection.findOne({ _id: new ObjectId(id) });
 
 			if (result) {
@@ -26,55 +35,94 @@ export default class WebsiteRecordModel extends Model<WebsiteRecord> {
 					label: result.label,
 					active: result.active,
 					tags: result.tags,
-                    latestGroupId: result.latestGroupId
+					latestGroupId: result.latestGroupId
 				} as WebsiteRecord;
 			}
-			return null;
+
+			return {
+				code: 404,
+				message: 'Website record not found'
+			} as Error;
 		} catch (error) {
-			console.log(error);
-			return null;
+			return {
+				code: 500,
+				message: 'Failed to get website record'
+			} as Error;
 		}
 	}
-    async update(id: string, updatedItem: WebsiteRecord): Promise<WebsiteRecord | null> {
-        try {
-            const result = await this.collection.updateOne(
-                { _id: new ObjectId(id) },
-                { $set: updatedItem }
-            );
-            if (result.modifiedCount > 0) {
-                return updatedItem;
-            }
-            return null;
-        } catch (error) {
-            console.log(error);
-            return null;
-        }
-    }
-    async delete(id: string): Promise<boolean> {
-        try {
-            const result = await this.collection.deleteOne({ _id: new ObjectId(id) });
-            return result.deletedCount > 0;
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
-    }
-    async getAll(): Promise<WebsiteRecord[]> {
-        try {
-            const result = await this.collection.find().toArray();
-            return result.map((item) => ({
-                id: item._id,
-                url: item.url,
-                periodicity: item.periodicity,
-                regexp: item.regexp,
-                label: item.label,
-                active: item.active,
-                tags: item.tags,
-                latestGroupId: item.latestGroupId
-            } as WebsiteRecord));
-        } catch (error) {
-            console.log(error);
-            return [];
-        }
-    }
+	async update(id: string, updatedItem: WebsiteRecord): Promise<WebsiteRecord | Error> {
+		try {
+			if (!ObjectId.isValid(id)) {
+				return {
+					code: 400,
+					message: 'Invalid Mongo ID'
+				} as Error;
+			}
+
+			const result = await this.collection.updateOne(
+				{ _id: new ObjectId(id) },
+				{ $set: updatedItem }
+			);
+
+			if (result.modifiedCount > 0) {
+				return updatedItem;
+			}
+			return {
+				code: 404,
+				message: 'Website record not found'
+			} as Error;
+		} catch (error) {
+			return {
+				code: 500,
+				message: 'Failed to update website record'
+			} as Error;
+		}
+	}
+	async delete(id: string): Promise<boolean | Error> {
+		try {
+			if (!ObjectId.isValid(id)) {
+				return {
+					code: 400,
+					message: 'Invalid Mongo ID'
+				} as Error;
+			}
+
+			const result = await this.collection.deleteOne({ _id: new ObjectId(id) });
+			if (result.deletedCount > 0) {
+				return true;
+			}
+			return {
+				code: 404,
+				message: 'Website record not found'
+			} as Error;
+		} catch (error) {
+			return {
+				code: 500,
+				message: 'Failed to delete website record'
+			} as Error;
+		}
+	}
+	async getAll(): Promise<WebsiteRecord[] | Error> {
+		try {
+			const result = await this.collection.find().toArray();
+			return result.map(
+				(item) =>
+					({
+						id: item._id,
+						url: item.url,
+						periodicity: item.periodicity,
+						regexp: item.regexp,
+						label: item.label,
+						active: item.active,
+						tags: item.tags,
+						latestGroupId: item.latestGroupId
+					} as WebsiteRecord)
+			);
+		} catch (error) {
+			return {
+				code: 500,
+				message: 'Failed to get all website records'
+			} as Error;
+		}
+	}
 }
