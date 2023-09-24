3e007fa773c995d4a61960e6122c227da88d11af
Mark Seliverstov <smdmrr00@gmail.com>
2023-09-24 11:07:29 +0200

    add controller

--- crawler/src/api/controller.ts
+++ crawler/src/api/controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { getRecordByID, WebsiteRecord, getExecutionsByOwnerIDAndGroupID } from "../db";
+import { WebsiteRecord } from "../db/model";
+import { getRecordByID } from "../db/api";
 import { crawlerManager } from "../crawler";
 
 export const startCrawling = async (req:Request, res: Response) => {
@@ -25,15 +26,8 @@ export const stopCrawling = async (req:Request, res: Response) => {
             message: `Record with id: ${recordId} not found!`
         });
     }
-
-    const execution = await getExecutionsByOwnerIDAndGroupID(record.id, record.latestGroupId);
-    if (execution === null) {
-        return res.status(404).json({
-            message: `Execution with ownerId: ${record.id} a groupId: ${record.latestGroupId} not found!`
-        });
-    }
     
-    crawlerManager.abortExecution(execution);
+    crawlerManager.abortExecutionRecord(record);
     return res.status(200).json({
         message: "Crawling was succesfully aborted"
     });
