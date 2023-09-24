cf2e876272a37d6608fe61cc19c3b7a520c3fcf2
Mark Seliverstov <smdmrr00@gmail.com>
2023-09-24 11:07:44 +0200

    add config

--- crawler/src/config.ts
+++ crawler/src/config.ts
@@ -1,3 +1,3 @@
 export const CrawlersCount = process.env.CRAWLER_COUNT ?? 4;
 export const Port = process.env.PORT ?? 5000;
-export const BasicMongoUrl = process.env.BASIC_MONGO_URL ?? "localhost:5050/";
+export const BasicMongoUrl = process.env.BASIC_MONGO_URL ?? "http://localhost:5050";
